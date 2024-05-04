e17e9c9a15aac48094091f8041229c79ce990bbb
nyat02 <rowo_2dt@yahoo.co.jp>
2024-05-04 17:56:14 +0900

    add main.go 4-15 learn[string, バッククォテーションで複数行, s[n]でn番目の文字をbyte型で取得, string(s[n])で文字に変換]

---
+++ src/sec4/5-string/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "Hello Golang"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	var si string = "300"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
+
+	fmt.Println(`test
+	test
+		test`)
+	fmt.Println("\"")
+	fmt.Println(`"`)
+
+	fmt.Println(s[0])
+	fmt.Println(string(s[0]))
+}
