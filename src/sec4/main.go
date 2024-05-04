b3c4d2f937578136992331286081fff43fb391fb
nyat02 <rowo_2dt@yahoo.co.jp>
2024-05-04 17:23:09 +0900

    add main.go 4-11 learn[int, int8(16,32,64),異なる型同士の演算不可, 型の調査, 型の変換]

---
+++ src/sec4/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 100
+	var i2 int64 = 200
+	fmt.Println(i + 50)
+	// fmt.Println(i + i2)
+
+	fmt.Printf("%T\n", i2)
+	fmt.Printf("%T\n", int32(i2))
+}
