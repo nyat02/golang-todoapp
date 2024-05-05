7a5d8f873ea7804d2cf32a3a8b1e6a2820004611
nyat02 <rowo_2dt@yahoo.co.jp>
2024-05-05 15:36:41 +0900

    add main.go 4-20 learn[interface, 宣言, どんな方でも入る, 各型の演算は利用できない]

---
+++ src/sec4/10-interface/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x interface{}
+	fmt.Println(x)
+
+	x = 1
+	fmt.Println(x)
+	x = "Hello"
+	fmt.Println(x)
+	x = [3]int{1, 2, 3}
+	fmt.Println(x)
+
+	x = 2
+	// fmt.Println(x + 3)
+}
