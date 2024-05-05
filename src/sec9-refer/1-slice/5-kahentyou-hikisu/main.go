c4de8c268ad5e7bd2be922d2ddefc44767f29b46
nyat02 <rowo_2dt@yahoo.co.jp>
2024-05-06 00:09:22 +0900

    add main.go 9-50 learn[可変長引数, 関数の引数->(s ...int), スライスで受け取る, 呼び出し側は任意の数の引数を渡せるSum(1,2,3,4), スライスも渡せるSum(sl...)]

---
+++ src/sec9-refer/1-slice/5-kahentyou-hikisu/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Sum(s ...int) int {
+	n := 0
+	for _, v := range s {
+		n += v
+	}
+	return n
+}
+
+func main() {
+	fmt.Println(Sum(1, 2, 3))
+
+	fmt.Println()
+
+	sl := []int{1, 2, 3}
+	fmt.Println(Sum(sl...))
+}
