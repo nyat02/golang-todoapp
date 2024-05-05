ae7a25c74c9b215bca0bd7b3e2f2abb1dc2457cd
nyat02 <rowo_2dt@yahoo.co.jp>
2024-05-05 17:33:38 +0900

    add main.go 8-35 learn[if文, 括弧不要, else if, 簡易文付きif文（スコープに注意）]

---
+++ src/sec8-statements/1-if/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 1
+
+	if a == 2 {
+		fmt.Println("two")
+	} else if a == 1 {
+		fmt.Println("one")
+	} else {
+		fmt.Println("I don't know")
+	}
+
+	if b := 100; b == 100 {
+		fmt.Println("one hundred")
+	}
+
+	x := 5
+	if x := 2; true {
+		fmt.Println(x)
+		//2
+	}
+	fmt.Println(x)
+
+}
